36ceff6039b628dfc264ab15f2b8aafed77475a3
Zac Clifton <43915749+Cliftonz@users.noreply.github.com>
2024-05-19 15:44:21 -0500

    Update docker and terraform configuration for AWS services
    The docker-compose.yml file has been updated to consolidate port mappings and add support for the Lambda service. Considerations for using LocalStack for system testing have been added to the terraform's providers.tf file by providing configurations to override AWS endpoints for S3, DynamoDB, and Lambda.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,17 @@
 provider "aws" {
   region = "us-east-2"
 
+#   # Uncomment if you want to use localstack to test the system
+#   skip_credentials_validation = true
+#   skip_metadata_api_check     = true
+#   s3_force_path_style         = true
+#
+#   endpoints {
+#     s3 = "http://localhost:4566"
+#     dynamodb = "http://localhost:4566"
+#     lambda = "http://localhost:4566"
+#   }
+
   default_tags {
     tags = {
       "project": "mediaConvert",
